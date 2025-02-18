7f055e593b2c4ddc7ecd757f428dcf482805ad6e
Daniel Ball <6998018+dgnball@users.noreply.github.com>
2025-02-18 13:24:46 +0000

    Hubspot scrape

--- agents.py
+++ agents.py
@@ -1,5 +1,6 @@
 from crewai import Agent
 
+from tools.scrape_hubspot import ScrapeHubspotIdeas
 from tools.search_tools import DuckDuckGoScrapeSearch, DuckDuckGoApiSearch
 
 """
@@ -19,6 +20,34 @@ class Agents:
             backstory="I can get you a summary of search results from Duck Duck Go",
             goal="Return the Duck Duck Go search results",
             tools=[DuckDuckGoScrapeSearch()],
+            verbose=True,
+        )
+
+    def hubspot_ideas_scrape_agent(self):
+        return Agent(
+            role="Hubspot ideas scraper",
+            backstory="I understand the Hubspot community page layout",
+            goal="""
+            Return a list of ideas including the following information:
+            * Date submitted
+            * Number of upvotes
+            * Title
+            * Description
+            * Category
+            * Read more link
+            
+            Format the list as a markdown table. Make sure the read more link is a full URL.
+            """,
+            tools=[ScrapeHubspotIdeas()],
+            verbose=True,
+        )
+
+    def summerise_an_idea_agent(self):
+        return Agent(
+            role="Summarise an idea",
+            backstory="I can read and digest the details of an idea webpage",
+            goal="Read the details of a page and summarise in less than 2-3 paragraphs",
+            tools=[ScrapeHubspotIdeas()],
             verbose=True,
         )
 
